e12cd851d44a31352725e94a2c43b16b34d96af7
felipedevpips-max <felipedevpips@gmail.com>
2026-02-03 18:34:02 -0300

    configurando api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://portifolio-fullstack-1w84.onrender.com/api",
 });
 
 export default api;
