8a3bdc5a021fbdb095af2978dd7dcf9e226dd7f6
felipedevpips-max <felipedevpips@gmail.com>
2026-02-03 16:57:42 -0300

    aplicando footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 import { Header } from "./components/Header";
 import Hero from "./components/Hero";
@@ -15,6 +16,7 @@ function App() {
       <Techs/>
       <Projects />
       <Contact/>
+      <Footer/>
 
     </>
   );
