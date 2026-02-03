8c97ebf994f9dafca46bb004259bdb0664710cd2
felipedevpips-max <felipedevpips@gmail.com>
2026-02-02 23:07:58 -0300

    Rotas atualizadas

--- api/src/controllers/techs.controller.js
+++ api/src/controllers/techs.controller.js
@@ -1,7 +1,22 @@
 function getTechs(req, res) {
   res.json([
-    { id: 1, name: "React", description: "Biblioteca para construção de interfaces" },
-    { id: 2, name: "Node.js", description: "Ambiente de execução JavaScript no servidor" },
-    { id: 3, name: "Docker", description: "Plataforma para containerização de aplicações" }
+    {
+      id: 1,
+      name: "React",
+      description: "Biblioteca para construção de interfaces",
+      icon: "react-icon.png",
+    },
+    {
+      id: 2,
+      name: "Node.js",
+      description: "Ambiente de execução JavaScript no servidor",
+      icon: "nodejs-icon.png",
+    },
+    {
+      id: 3,
+      name: "Docker",
+      description: "Plataforma para containerização de aplicações",
+      icon: "docker-icon.png",
+    },
   ]);
 }
